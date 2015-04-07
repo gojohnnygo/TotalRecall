44c76b443db664530748dd7cb97afba0522ee108
Johnny Nguyen <hellojohnnguyen@gmail.com>
2015-04-07 16:11:28 -0700

    stuff

--- data.js
+++ data.js
@@ -1,78 +1,96 @@
 var totalRecall = {};
 
-totalRecall.sets = ["hr26","hr27"]; // all valid sets
-totalRecall.defaultSets = ["hr26","hr27"];  // default sets to load on page load
 totalRecall.hr26 = [
-["Ansel Rosenberg","http://cdn.defi.co/hr26/scaled/ansel_rosenberg.jpg"],
-["Cheng Tian","http://cdn.defi.co/hr26/scaled/cheng_tian.jpg"],
-["Cindy Cheung","http://cdn.defi.co/hr26/scaled/cindy_cheung.jpg"],
-["Darren Wong","http://cdn.defi.co/hr26/scaled/darren_wong.jpg"],
-["David Kiesewetter","http://cdn.defi.co/hr26/scaled/david_kiesewetter.jpg"],
-["Derek Boero","http://cdn.defi.co/hr26/scaled/derek_boero.jpg"],
-["Eden Mazzola","http://cdn.defi.co/hr26/scaled/eden_mazzola.jpg"],
-["Edoe Cohen","http://cdn.defi.co/hr26/scaled/edoe_cohen.jpg"],
-["Elvio Cavalcante","http://cdn.defi.co/hr26/scaled/elvio_cavalcante.jpg"],
-["Eric Outterson","http://cdn.defi.co/hr26/scaled/eric_outterson.jpg"],
-["Faye Guo","http://cdn.defi.co/hr26/scaled/faye_guo.jpg"],
-["Frank Bowers","http://cdn.defi.co/hr26/scaled/frank_bowers.jpg"],
-["George Lam","http://cdn.defi.co/hr26/scaled/george_lam.jpg"],
-["Henry Mollman","http://cdn.defi.co/hr26/scaled/henry_mollman.jpg"],
-["Jimmy Tjandra","http://cdn.defi.co/hr26/scaled/jimmy_tjandra.jpg"],
-["John Pizzo","http://cdn.defi.co/hr26/scaled/john_pizzo.jpg"],
-["Kaivon Afsari","http://cdn.defi.co/hr26/scaled/kaivon_afsari.jpg"],
-["Ken Kang","http://cdn.defi.co/hr26/scaled/ken_kang.jpg"],
-["Kenny Langston","http://cdn.defi.co/hr26/scaled/kenny_langston.jpg"],
-["Khoa Phan","http://cdn.defi.co/hr26/scaled/khoa_phan.jpg"],
-["Kir Jarchow","http://cdn.defi.co/hr26/scaled/kir_jarchow.jpg"],
-["Kyle Hilton","http://cdn.defi.co/hr26/scaled/kyle_hilton.jpg"],
-["Kyle Shockey","http://cdn.defi.co/hr26/scaled/kyle_shockey.jpg"],
-["Mark Martinez","http://cdn.defi.co/hr26/scaled/mark_martinez.jpg"],
-["Matt Brown","http://cdn.defi.co/hr26/scaled/matt_brown.jpg"],
-["Moriah Kreeger","http://cdn.defi.co/hr26/scaled/moriah_kreeger.jpg"],
-["Natalie Spencer","http://cdn.defi.co/hr26/scaled/natalie_spencer.jpg"],
-["Pranav Kalvapalle","http://cdn.defi.co/hr26/scaled/pranav_kalvapalle.jpg"],
-["Quest Henkart","http://cdn.defi.co/hr26/scaled/quest_henkart.jpg"],
-["Raymond Luong","http://cdn.defi.co/hr26/scaled/raymond_luong.jpg"],
-["Rocky Tang","http://cdn.defi.co/hr26/scaled/rocky_tang.jpg"],
-["Sunshine Yin","http://cdn.defi.co/hr26/scaled/sunshine_yin.jpg"],
-["Yuriy Bash","http://cdn.defi.co/hr26/scaled/yuriy_bash.jpg"]];
+  ["26", "male",   "Caucasian", "Ansel Rosenberg","http://cdn.defi.co/hr26/scaled/ansel_rosenberg.jpg"],
+  ["26", "male",   "Asian",     "Cheng Tian","http://cdn.defi.co/hr26/scaled/cheng_tian.jpg"],
+  ["26", "male",   "Asian",     "Cindy Cheung","http://cdn.defi.co/hr26/scaled/cindy_cheung.jpg"],
+  ["26", "male",   "Asian",     "Darren Wong","http://cdn.defi.co/hr26/scaled/darren_wong.jpg"],
+  ["26", "male",   "Caucasian", "David Kiesewetter","http://cdn.defi.co/hr26/scaled/david_kiesewetter.jpg"],
+  ["26", "male",   "Caucasian", "Derek Boero","http://cdn.defi.co/hr26/scaled/derek_boero.jpg"],
+  ["26", "male",   "Caucasian", "Eden Mazzola","http://cdn.defi.co/hr26/scaled/eden_mazzola.jpg"],
+  ["26", "male",   "Caucasian", "Edoe Cohen","http://cdn.defi.co/hr26/scaled/edoe_cohen.jpg"],
+  ["26", "male",   "Caucasian", "Elvio Cavalcante","http://cdn.defi.co/hr26/scaled/elvio_cavalcante.jpg"],
+  ["26", "male",   "Caucasian", "Eric Outterson","http://cdn.defi.co/hr26/scaled/eric_outterson.jpg"],
+  ["26", "female", "Asian",     "Faye Guo","http://cdn.defi.co/hr26/scaled/faye_guo.jpg"],
+  ["26", "male",   "Caucasian", "Frank Bowers","http://cdn.defi.co/hr26/scaled/frank_bowers.jpg"],
+  ["26", "male",   "Asian",     "George Lam","http://cdn.defi.co/hr26/scaled/george_lam.jpg"],
+  ["26", "male",   "Caucasian", "Henry Mollman","http://cdn.defi.co/hr26/scaled/henry_mollman.jpg"],
+  ["26", "male",   "Asian",     "Jimmy Tjandra","http://cdn.defi.co/hr26/scaled/jimmy_tjandra.jpg"],
+  ["26", "male",   "Caucasian", "John Pizzo","http://cdn.defi.co/hr26/scaled/john_pizzo.jpg"],
+  ["26", "male",   "Asian",     "Kaivon Afsari","http://cdn.defi.co/hr26/scaled/kaivon_afsari.jpg"],
+  ["26", "male",   "Asian",     "Ken Kang","http://cdn.defi.co/hr26/scaled/ken_kang.jpg"],
+  ["26", "male",   "Caucasian", "Kenny Langston","http://cdn.defi.co/hr26/scaled/kenny_langston.jpg"],
+  ["26", "male",   "Asian",     "Khoa Phan","http://cdn.defi.co/hr26/scaled/khoa_phan.jpg"],
+  ["26", "female", "Caucasian", "Kir Jarchow","http://cdn.defi.co/hr26/scaled/kir_jarchow.jpg"],
+  ["26", "male",   "Caucasian", "Kyle Hilton","http://cdn.defi.co/hr26/scaled/kyle_hilton.jpg"],
+  ["26", "male",   "Caucasian", "Kyle Shockey","http://cdn.defi.co/hr26/scaled/kyle_shockey.jpg"],
+  ["26", "male",   "Caucasian", "Mark Martinez","http://cdn.defi.co/hr26/scaled/mark_martinez.jpg"],
+  ["26", "male",   "Caucasian", "Matt Brown","http://cdn.defi.co/hr26/scaled/matt_brown.jpg"],
+  ["26", "female", "Caucasian", "Moriah Kreeger","http://cdn.defi.co/hr26/scaled/moriah_kreeger.jpg"],
+  ["26", "female", "Caucasian", "Natalie Spencer","http://cdn.defi.co/hr26/scaled/natalie_spencer.jpg"],
+  ["26", "male",   "Asian",     "Pranav Kalvapalle","http://cdn.defi.co/hr26/scaled/pranav_kalvapalle.jpg"],
+  ["26", "male",   "Caucasian", "Quest Henkart","http://cdn.defi.co/hr26/scaled/quest_henkart.jpg"],
+  ["26", "male",   "Asian",     "Raymond Luong","http://cdn.defi.co/hr26/scaled/raymond_luong.jpg"],
+  ["26", "male",   "Asian",     "Rocky Tang","http://cdn.defi.co/hr26/scaled/rocky_tang.jpg"],
+  ["26", "female", "Asian",     "Sunshine Yin","http://cdn.defi.co/hr26/scaled/sunshine_yin.jpg"],
+  ["26", "male",   "Caucasian", "Yuriy Bash","http://cdn.defi.co/hr26/scaled/yuriy_bash.jpg"]
+];
 
 totalRecall.hr27 = [
-
-["Andy Sponring","http://cdn.defi.co/hr27/andy_sponring.jpg"],
-["Andy Tran","http://cdn.defi.co/hr27/andy_tran.jpg"],
-["Anuj Tomar","http://cdn.defi.co/hr27/anuj_tomar.jpg"],
-["Austin Liu","http://cdn.defi.co/hr27/austin_liu.jpg"],
-["Brant Choate","http://cdn.defi.co/hr27/brant_choate.jpg"],
-["Brian Liu","http://cdn.defi.co/hr27/brian_liu.jpg"],
-["Charlie Hwang","http://cdn.defi.co/hr27/charlie_hwang.jpg"],
-["Dave Grundfest","http://cdn.defi.co/hr27/dave_grundfest.jpg"],
-["David Hopper","http://cdn.defi.co/hr27/david_hopper.jpg"],
-["David Trinh","http://cdn.defi.co/hr27/david_trinh.jpg"],
-["Derek Sakamoto","http://cdn.defi.co/hr27/derek_sakamoto.jpg"],
-["Eddie Kong","http://cdn.defi.co/hr27/eddie_kong.jpg"],
-["Irfan Baqui","http://cdn.defi.co/hr27/irfan_baqui.jpg"],
-["John Winstead","http://cdn.defi.co/hr27/john_winstead.jpg"],
-["John Yeglinski","http://cdn.defi.co/hr27/john_yeglinski.jpg"],
-["Johnny Nguyen","http://cdn.defi.co/hr27/johnny_nguyen.jpg"],
-["Joseph Lin","http://cdn.defi.co/hr27/joseph_lin.jpg"],
-["Justin Webb","http://cdn.defi.co/hr27/justin_webb.jpg"],
-["Katrina Uychaco","http://cdn.defi.co/hr27/katrina_uychaco.jpg"],
-["Kevin Huang","http://cdn.defi.co/hr27/kevin_huang.jpg"],
-["Kurt Bartholemew","http://cdn.defi.co/hr27/kurt_bartholomew.jpg"],
-["Lauren Spiegel","http://cdn.defi.co/hr27/lauren_spiegel.jpg"],
-["Lu Yin","http://cdn.defi.co/hr27/lu_yin.jpg"],
-["Mark Bennett","http://cdn.defi.co/hr27/mark_bennett.jpg"],
-["Mark Tausch","http://cdn.defi.co/hr27/mark_tausch.jpg"],
-["Melanie Gin","http://cdn.defi.co/hr27/melanie_gin.jpg"],
-["Michael Aristarkhov","http://cdn.defi.co/hr27/michael_aristarkhov.jpg"],
-["Mike Harris","http://cdn.defi.co/hr27/mike_harris.jpg"],
-["Mike Yao","http://cdn.defi.co/hr27/mike_yao.jpg"],
-["Nathan Bailey","http://cdn.defi.co/hr27/nathan_bailey.jpg"],
-["Nimi Dharithreesan","http://cdn.defi.co/hr27/nimi_dharithreesan.jpg"],
-["Ron Tsui","http://cdn.defi.co/hr27/ron_tsui.jpg"],
-["Steven Shyun","http://cdn.defi.co/hr27/steven_shyun.jpg"],
-["Vince Tam","http://cdn.defi.co/hr27/vince_tam.jpg"],
-["Vivek Tumrukota","http://cdn.defi.co/hr27/vivek_tumrukota.jpg"],
-["Zach Fischmann","http://cdn.defi.co/hr27/zack_fischmann.jpg"]
+  ["27", "male",   "Asian",           "Andy Sponring",        "http://cdn.defi.co/hr27/andy_sponring.jpg"],
+  ["27", "male",   "Asian",           "Andy Tran",            "http://cdn.defi.co/hr27/andy_tran.jpg"],
+  ["27", "male",   "Asian",           "Anuj Tomar",           "http://cdn.defi.co/hr27/anuj_tomar.jpg"],
+  ["27", "male",   "Asian",           "Austin Liu",           "http://cdn.defi.co/hr27/austin_liu.jpg"],
+  ["27", "male",   "Caucasian",       "Brant Choate",         "http://cdn.defi.co/hr27/brant_choate.jpg"],
+  ["27", "male",   "Asian",           "Brian Liu",            "http://cdn.defi.co/hr27/brian_liu.jpg"],
+  ["27", "male",   "Asian",           "Charlie Hwang",        "http://cdn.defi.co/hr27/charlie_hwang.jpg"],
+  ["27", "male",   "Caucasian",       "Dave Grundfest",       "http://cdn.defi.co/hr27/dave_grundfest.jpg"],
+  ["27", "male",   "Caucasian",       "David Hopper",         "http://cdn.defi.co/hr27/david_hopper.jpg"],
+  ["27", "male",   "Asian",           "David Trinh",          "http://cdn.defi.co/hr27/david_trinh.jpg"],
+  ["27", "male",   "Asian",           "Derek Sakamoto",       "http://cdn.defi.co/hr27/derek_sakamoto.jpg"],
+  ["27", "male",   "Asian",           "Eddie Kong",           "http://cdn.defi.co/hr27/eddie_kong.jpg"],
+  ["27", "male",   "Asian",           "Irfan Baqui",          "http://cdn.defi.co/hr27/irfan_baqui.jpg"],
+  ["27", "male",   "Caucasian",       "John Winstead",        "http://cdn.defi.co/hr27/john_winstead.jpg"],
+  ["27", "male",   "Asian",           "John Yeglinski",       "http://cdn.defi.co/hr27/john_yeglinski.jpg"],
+  ["27", "male",   "Asian",           "Johnny Nguyen",        "http://cdn.defi.co/hr27/johnny_nguyen.jpg"],
+  ["27", "male",   "Asian",           "Joseph Lin",           "http://cdn.defi.co/hr27/joseph_lin.jpg"],
+  ["27", "male",   "Afican American", "Justin Webb",          "http://cdn.defi.co/hr27/justin_webb.jpg"],
+  ["27", "female", "Asian",           "Katrina Uychaco",      "http://cdn.defi.co/hr27/katrina_uychaco.jpg"],
+  ["27", "male",   "Asian",           "Kevin Huang",          "http://cdn.defi.co/hr27/kevin_huang.jpg"],
+  ["27", "male",   "Caucasian",       "Kurt Bartholemew",     "http://cdn.defi.co/hr27/kurt_bartholomew.jpg"],
+  ["27", "female", "Asian",           "Lauren Spiegel",       "http://cdn.defi.co/hr27/lauren_spiegel.jpg"],
+  ["27", "male",   "Asian",           "Lu Yin",               "http://cdn.defi.co/hr27/lu_yin.jpg"],
+  ["27", "male",   "Caucasian",       "Mark Bennett",         "http://cdn.defi.co/hr27/mark_bennett.jpg"],
+  ["27", "male",   "Caucasian",       "Mark Tausch",          "http://cdn.defi.co/hr27/mark_tausch.jpg"],
+  ["27", "female", "Asian",           "Melanie Gin",          "http://cdn.defi.co/hr27/melanie_gin.jpg"],
+  ["27", "male",   "Caucasian",       "Michael Aristarkhov",  "http://cdn.defi.co/hr27/michael_aristarkhov.jpg"],
+  ["27", "male",   "Caucasian",       "Mike Harris",          "http://cdn.defi.co/hr27/mike_harris.jpg"],
+  ["27", "male",   "Asian",           "Mike Yao",             "http://cdn.defi.co/hr27/mike_yao.jpg"],
+  ["27", "male",   "Caucasian",       "Nathan Bailey",        "http://cdn.defi.co/hr27/nathan_bailey.jpg"],
+  ["27", "female", "Asian",           "Nimi Dharithreesan",   "http://cdn.defi.co/hr27/nimi_dharithreesan.jpg"],
+  ["27", "male",   "Asian",           "Ron Tsui",             "http://cdn.defi.co/hr27/ron_tsui.jpg"],
+  ["27", "male",   "Asian",           "Steven Shyun",         "http://cdn.defi.co/hr27/steven_shyun.jpg"],
+  ["27", "male",   "Asian",           "Vince Tam",            "http://cdn.defi.co/hr27/vince_tam.jpg"],
+  ["27", "male",   "Asian",           "Vivek Tumrukota",      "http://cdn.defi.co/hr27/vivek_tumrukota.jpg"],
+  ["27", "male",   "Caucasian",       "Zach Fischmann",       "http://cdn.defi.co/hr27/zack_fischmann.jpg"]
 ];
+
+totalRecall.data = (function() {
+  var hr = totalRecall.hr26.concat(totalRecall.hr27);
+  var storage = [];
+
+  hr.forEach(function(el, i) {
+    var obj = {};
+
+    obj.hr     = el[0];
+    obj.gender = el[1];
+    obj.race   = el[2];
+    obj.first  = el[3].split(' ')[0];
+    obj.last   = el[3].split(' ')[1];
+    obj.url    = el[4];
+
+    storage.push(obj);
+  });
+
+  return storage;
+})();
--- totalRecall.js
+++ totalRecall.js
@@ -1,131 +1,57 @@
-$( document ).ready(function() {
+$(document).ready(function() {
+  var app = {};
+  
+  app.score = [];
+  app.avatarIndex = 0;
+  app.buttonIndex = 0;
 
-  totalRecall.sets.forEach(function(val){ // drop a button in .set-selection for every set we have.
-    var $input = $('<input/>', { type: "button", id: val, value: val, class: "tr-selectButton btn btn-default"});
-    $input.appendTo($(".set-selection"));
+  $('.buttons').on('click', '.btn', function() {
+    avatarAnimateOut();
   });
 
-$('.tr-selectButton').each(function(){
-  if ( totalRecall.defaultSets.indexOf($(this).attr('id') !== -1)) {
-    $(this).addClass('btn-success').removeClass('btn-default');
-  }
-})
+  
 
-
-  var usableData = [];
-  totalRecall.defaultSets.forEach(function(val){
-    usableData = usableData.concat(totalRecall[val]);
-  })
-
-  var allData = usableData.slice(0);
-
-  var correct = newPrompt();
-  $('#next').toggle();
-  var score = 0, total = 0;
-
-  function newPrompt(){
-    var usedIndexes = [];
-    var correctIndex = Math.ceil(Math.random() * 3);
-    var person = getRandomPerson(usedIndexes);
-    $('.img-square').attr("src", person[1]);
-    $('#' + correctIndex).text(person[0]);
-
-    //cycle through the 3 buttons
-
-    for (var i = 1; i < 4; i++){
-      //make sure you don't overwrite the button with the correct answer
-      if (i !== correctIndex){
-        //populate buttons
-        var person = getRandomPersonFromData(usedIndexes);
-        $('#' + i).text(person[0]);
-      }
-    }
-    return correctIndex;
+  var avatarAnimateIn = function() {
+    app.avatarIndex++;
+    $('#avatar').attr('src', totalRecall.data[app.avatarIndex].url);
+    $('#avatar').toggleClass('bounceInLeft').toggleClass('bounceOutLeft');
   };
 
-  function getRandomPerson(usedIndexes){
-    var index = Math.floor((Math.random() * usableData.length));
-    if(usedIndexes.indexOf(index) === -1) {
-      usedIndexes.push(index);
-      return usableData[index];
-    } else {
-      return getRandomPerson(usedIndexes);
-    }
-  }
+  var avatarAnimateOut = function() {
+    $('#avatar').toggleClass('bounceInLeft').toggleClass('bounceOutLeft');
+    // $('#avatar').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', avatarAnimateIn);
+  };
 
-  function getRandomPersonFromData(usedIndexes){
-    var index = Math.floor((Math.random() * allData.length));
-    if(usedIndexes.indexOf(index) === -1) {
-      usedIndexes.push(index);
-      return allData[index];
-    } else {
-      return getRandomPersonFromData(usedIndexes);
-    }
-  }
-  //Event handler for clicking on button. Update scores, calls paintButtons method.
-  $('.btn.btn-default.btn-lg.btn-block').click(function(){
-    if ($(this).attr('id') === correct.toString()){
-      score++;
-      var name = $(this).text(); // string of correct person's name
-      var indexToRemove;
-      usableData.forEach(function(person, i){ // 
-        console.log(person[0] + " " + person[1] + " " + person);
-        if(person[0] === name) {
-          indexToRemove = i;
-        }
-      });
-      console.log("--------------");
-      var removed = usableData.splice(indexToRemove,1);
-    }
-    total++;
-    $(this).css("background-color","#FF0000");
-    $('.score').text(score + ' out of ' + total + ' (' + Math.round((score / total * 100)) + '%)');
-    paintButtons();
-  });
-
-  $('#playAgainButton').click(function(){
-    location.reload(); // this is hacky, there's definitely a better way to do this
-  });
-
-  //Colors the correct button button green, disables all, shows next.
-  function paintButtons(){
-    for (var i = 1; i < 4; i++){
-      var button = $('#' + i);
-      if (button.attr('id') === correct.toString()){
-        $(button).css("background-color","#0CFA1C");
-        $(button).prop("disabled",true);
-      } else {
-        $(button).prop("disabled",true);
+  var animateNames = function() {
+    var timer = setInterval(function() {
+      app.buttonIndex++;
+      
+      if (app.buttonIndex === 4) {
+        app.buttonIndex = 0;
+        clearTimeout(timer);
       }
-    }
-    $('#next').toggle();
-  }
 
-  $('#next').click(function(){
-    if(usableData.length > 0) {
-      correct = newPrompt();
-      for (var i = 1; i < 4; i++){
-        var button = $('#' + i);
-        $(button).css("background-color", "white");
-        $(button).prop("disabled",false);
-      } 
-    } else {
-        // show end-game div
-        $('#gameplay').hide();
-        $('#endgame').show();
-        //insert the final score into the endgame div
-        $('#finalScore').text(score + ' out of ' + total + ' (' + Math.round((score / total * 100)) + '%)');
+      $(".buttons button:nth-child(" + app.buttonIndex + ")")
+        .toggleClass('bounceInRight')
+        .toggleClass('bounceOutRight');
+    }, 50);
+  };
 
+  var init = function() {
+    $('#avatar').attr('src', totalRecall.data[0].url);
+    $('#avatar').addClass('bounceInLeft');
 
+    var timer = setInterval(function() {
+      app.buttonIndex++;
+      
+      if (app.buttonIndex === 4) {
+        app.buttonIndex = 0;
+        clearTimeout(timer);
       }
-    $(this).toggle();
-  });
 
-  $('.tr-selectButton').click(function(){
-    $(this).toggleClass('btn-success btn-default');
-    usableData = [];
-    $('.tr-selectButton.btn-success').each(function(){
-      usableData = usableData.concat(totalRecall[$(this).attr('id')]);
-    });
-  });
+       $(".buttons button:nth-child(" + app.buttonIndex + ")").css("visibility", "visible").addClass('bounceInRight');
+    }, 50);
+  };
+
+  init();
 });
